6926b0cd1d792e3c2f53e5c859c0f420a6ecd991
hongyinull <80037820+hongyinull@users.noreply.github.com>
2025-10-03 19:00:59 +0800

    更新index.html以修正資源路徑並新增服務工作者註冊功能

---
+++ smartorder/sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'smartorder-cache-v1';
+const CORE_ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((key) => key !== CACHE_NAME ? caches.delete(key) : null))).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+  // Prefer network, fallback to cache
+  event.respondWith(
+    fetch(event.request).then((res) => {
+      const resClone = res.clone();
+      caches.open(CACHE_NAME).then((cache) => cache.put(event.request, resClone));
+      return res;
+    }).catch(() => caches.match(event.request))
+  );
+});
+
